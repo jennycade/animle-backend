3ccbda7ff0707c88f5d6428ac82f530440a7f65a
Jenny Zonka <jennycade@gmail.com>
2022-06-21 10:13:21 -0700

    Started on nwk processing recursion logic

--- data/processNwk.py
+++ data/processNwk.py
@@ -1,3 +1,43 @@
 # recursively peel off outer parentheses
 # sibling, (descendants)'nodeName':millionYears
 # if !isNumeric(nodeName) -> make a species too
+
+import re
+
+# first: get recursion working
+def processNwk(nwkPath):
+  nwkFile = open(nwkPath, 'r')
+
+  # preprocess
+  nwk = nwkFile.read()[:-2] # strip ; and \n
+
+  print('---------------------------------------------')
+  print(nwk)
+  print('---------------------------------------------')
+
+  # recursion
+  printLevel(nwk)
+
+def printLevel(nwk: str):
+  if nwk[0] == '(' and nwk[-1] == ')':
+    print('root')
+    printLevel(nwk[1:-1])
+  else:
+    pattern = r'([^\(]*)(\(.+\))(.*)'
+    match = re.match(pattern, nwk)
+    if match:
+      print('---------------------------------------------')
+      print(f'Before (:  {match.group(1)}')
+      print(f'After ) :   {match.group(3)}')
+      printLevel(match.group(2))
+      # doesn't drill all the way down
+  
+
+
+    
+
+
+
+  
+
+processNwk('/Users/jennyzonka/Code/animle-backend/data/Ursidae_species.nwk')
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+// routes
+// get gameboard state
+// post guess
+// log in
+// get user stats
+// get species list (for filterable list)
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/guess')
 
 module.exports = router;
