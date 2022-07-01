5c98636643095ea103a5bfe9455ae6beb2232f77
Jenny Zonka <jennycade@gmail.com>
2022-07-01 15:26:22 -0700

    Added notes on routes needed

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,9 @@ router.get('/', function(req, res, next) {
   res.send('send a resource');
 });
 
+// GET all species
 router.get('/species', async function(req, res, next) {
+  // TODO: consider cacheing
   try {
     // get the species names
     const animals = await Animal.find().sort('speciesName').exec();
@@ -25,6 +27,20 @@ router.get('/species', async function(req, res, next) {
   }
 });
 
-router.post('/guess')
+// POST a guess
+router.post('/guess', async function(req, res, next) {
+  // check user
+  // check game: make a game, continue a game, or game is over?
+  // validate guess
+  // check for guess in cache
+  // calculate distance and add to cache
+  // return distance and game condition
+  res.json('blah');
+});
+
+// GET time to next game
+router.get('/nextGameTime', async function(req, res, next) {
+  res.json('blah');
+});
 
 module.exports = router;
